86dcc2f0201cd82379b7d5cb116a0db597f1e113
DevNakatori <dev@nakatori.agency>
2024-08-30 17:29:14 +0530

    fix - button(Apply) style in cart

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -123,7 +123,7 @@ function CartCheckoutActions({checkoutUrl}) {
   return (
     <div>
       <a className="yellow-btn checkout" href={checkoutUrl} target="_self">
-        <p>Weiter Zur Kasse</p>
+        <p>Weiter zur Kasse</p>
       </a>
       <br />
     </div>
